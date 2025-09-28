058defa4c0b253acf69777c5198a3130672d5697
Rajendranaidu777 <rajendranaidu497@gmail.com>
2025-09-28 15:07:04 +0530

    second commit

---
+++ src/test/java/TestBase/Basetest2.java
@@ -0,0 +1,11 @@
+package TestBase;
+
+import org.testng.annotations.Test;
+
+public class Basetest2 {
+	@Test
+	public void testgit() {
+		System.out.println("test for adding git Round 2");
+	}
+
+}
